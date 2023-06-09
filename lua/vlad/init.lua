97c2af22deb4d4bd9fe90e1ba0aad5517450b980
Vlad M <vlad.milytin@gmail.com>
2023-06-09 20:18:19 +0300

    More commands, installed refactor, fixed grep

--- lua/vlad/init.lua
+++ lua/vlad/init.lua
@@ -1,2 +1,3 @@
 require("vlad.remap")
+require("vlad.setter")
 print("hello from vlad")
--- lua/vlad/remap.lua
+++ lua/vlad/remap.lua
@@ -1,27 +1,10 @@
 vim.g.mapleader = " "
-vim.api.nvim_command('set nonumber')
-vim.api.nvim_command('set scrolloff=10')
---vim.api.nvim_command('set linenumber')
-vim.api.nvim_command('set relativenumber')
-vim.api.nvim_command('set number')
-vim.api.nvim_command('set relativenumber')
-vim.api.nvim_command('set relativenumber')
-vim.api.nvim_command('set relativenumber')
-vim.api.nvim_command('set relativenumber')
-vim.api.nvim_command('set tabstop=4')
-vim.api.nvim_command('set shiftwidth=4')
-vim.api.nvim_command('set smartindent')
-vim.api.nvim_command('set autoindent')
-vim.api.nvim_command('set smartcase')
---set showmode
---set showcmd
---set smartcase
---set incsearch
---set hlsearch
--- set visualbell
---set clipboard+=unnamed
---let mapleader = " "
 
 vim.keymap.set("n", "<leader>wq", vim.cmd.Ex)
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+
+--Window manupulation
+vim.keymap.set("n", "<leader>ws", vim.cmd.split)
+vim.keymap.set("n", "<leader>wv", vim.cmd.vsp)
